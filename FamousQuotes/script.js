d656031f9a4c89de4d249298e15afe41197bdb38
TarekBarodi <tarek.barodi@gmail.com>
2020-01-15 13:14:25 +0100

    done

---
+++ FamousQuotes/script.js
@@ -0,0 +1,31 @@
+const simpleQuotes = [
+  {
+    name: "author number 1",
+    quote: "quote number 1"
+  },
+  {
+    name: "author number 2",
+    quote: "quote number 2"
+  },
+  {
+    name: "author number 3",
+    quote: "quote number 3"
+  },
+  {
+    name: "author number 4",
+    quote: "quote number 4"
+  }
+];
+
+const quoteBtn = document.querySelector("#quoteBtn");
+const quoteAuthor = document.querySelector("#quoteAuthor");
+const quote = document.querySelector("#quote");
+
+quoteBtn.addEventListener("click", displayQuote);
+
+function displayQuote() {
+  let number = Math.floor(Math.random() * simpleQuotes.length);
+  //console.log(number);
+  quoteAuthor.innerHTML = simpleQuotes[number].name;
+  quote.innerHTML = simpleQuotes[number].quote;
+}
